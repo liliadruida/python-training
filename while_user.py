bd1f0957f4eac7ba972f795a8ebec9cb3f441c10
liliadruida <toteme3@gmail.com>
2022-09-06 16:13:43 +0200

    dried up code

--- while_user.py
+++ while_user.py
@@ -1,7 +1,11 @@
 # A felhasználótól kérjetek be egy számot és írjátok ki a kétszeresét.
 # Ezt egészen addig ismételjétek, amíg a felhasználó 0-át nem ír be.
 
-number = int(input("Kérlek írj be egy számot: "))
+# DRY - don't repeat yourself
+
+#number = int(input("Kérlek írj be egy számot: "))
+number = 100
 while number > 0:
-    print(number * 2)
     number = int(input("Kérlek írj be egy számot: "))
+    print(number * 2)
+    
