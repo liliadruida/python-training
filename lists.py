e7c80fc97e221b814d0e5be99167a1b574f0f991
liliadruida <toteme3@gmail.com>
2022-09-06 14:57:20 +0200

    debug

--- lists.py
+++ lists.py
@@ -3,3 +3,43 @@ names = ["Joe", "Jack", "Jane"]
 # végigiterálunk a names változó elemein
 for name in names:
     print(name)
+
+# enumerate függvényt kell használni, ha az indexhez szeretnénk hozzáférni
+for i, name in enumerate(names):
+    print(i)
+    print(name)
+
+counter = 10
+for name in names:
+    print(counter)
+    print(name)
+    counter += 2
+
+# 1. Írd ki az első három hónap nevét egymás alá!
+# 2. Írd ki, hogy "Az év egyik hónapja január." az első három hónappal!
+# 3. Hozz létre egy listát a 3, 7, 9, 13 számokkal! Add össze a listában
+# lévő számokat!
+
+numbers = [3, 7, 9, 13]
+for number in numbers:
+    print(number)
+
+# 1. Írd ki az első három hónap nevét egymás alá!
+months = ["január", "február", "március", "április"]
+for month in months:
+    print(month)
+
+# 2. Írd ki, hogy "Az év egyik hónapja január." az első három hónappal!
+months = ["január", "február", "március", "április"]
+for month in months:
+    print(f"Az év egyik hónapja {month}")
+
+# 3. Hozz létre egy listát a 3, 7, 9, 13 számokkal! Add össze a listában
+# lévő számokat!
+
+numbers = [3, 7, 9, 13]
+sum = 0
+for number in numbers:
+    print(number)
+    sum += number
+print(sum)
---
+++ sum_list.py
@@ -0,0 +1,7 @@
+numbers = [3, 7, 9, 13]
+sum = 0
+for number in numbers:
+    print(number)
+    sum += number
+print(sum)
+
