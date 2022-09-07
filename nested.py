83a6ca33b5018a1e14258d204b9053b856cf06ab
liliadruida <toteme3@gmail.com>
2022-09-07 09:48:02 +0200

    completed task

---
+++ nested.py
@@ -0,0 +1,32 @@
+# Ciklusok egymásba ágyazása
+#for i in range(3):
+#    for j in range(5, 10):
+#        print(f"{i} - {j}")
+
+# Írjátok ki a 10x10-es szorzótáblát!
+#for i in range(1, 11):
+#    for j in range(1, 11):
+#        result = i * j
+#        print(f"{i} x {j} = {result}")
+
+# Írjuk ki a szorzótáblát négyzet alakban!
+#line = ""  # üres string, 0 karaktert tartalmazó string, 0 hosszú string
+#for i in range(1, 11):
+#    line += str(i) + " "
+#print(line)
+
+#for i in range(1, 11):
+#    line = ""
+#    for j in range(1,11):
+#        line += str(i * j) + " "
+#    print(line)
+
+# Ciklusban lehet elágazás is
+for i in range(1,11):
+    line = ""
+    for j in range(1,11):
+        result = i * j
+        if result < 10:
+            line += " "
+        line += str(result) + " "
+    print(line)
