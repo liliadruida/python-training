ef34ae3fdeacbed6670445d90b66a35d4851828b
liliadruida <toteme3@gmail.com>
2022-09-07 11:00:58 +0200

    mai feladatok

---
+++ only_positive.py
@@ -0,0 +1,16 @@
+# Hozzatok létre egy listát egész számokkal! Adjátok össze a pozitív számokat!
+
+numbers = [23, 0, 54, 76, 0, -10, 54, 0, -21, 0, -35, 10, 0]
+sum = 0
+for n in numbers:
+    if n > 0:
+        sum += n
+print(sum)
+
+# Számoljuk meg, és írjuk ki, hogy a listában hány nulla szerepel!
+
+count = 0
+for n in numbers:
+    if n == 0:
+        count += 1
+print(count)
---
+++ selection_adv.py
@@ -0,0 +1,33 @@
+#n = 24
+#if n % 2 == 0:
+#    print("páros")
+#    print("szám")
+#else:
+#    print("páratlan")
+#    print("szám")
+
+#n = 0
+#if n > 0:
+#    print("pozitív")
+#elif n == 0:
+#    print("nulla")
+#else:
+#    print("negatív")
+
+# Írd ki az n szám abszolútértékét!
+n = -24
+if n >= 0:
+    print(n)
+else:
+    # print = (n * -1)
+    print(-n)
+
+
+# Legyen két szám az i és j változóban, és írd ki közülük a kisebbet!
+i = 10
+j = 6
+
+if i > j:
+    print(j)
+else:
+    print(i)
